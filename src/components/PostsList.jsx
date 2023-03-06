5e54e8b2afe3a6c184dc81983d602e6d05fa6126
Alan Pecenkovic <alanquad@yahoo.com>
2023-03-06 16:23:42 +0100

    lifting the state up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
       <ul className={styles.posts}>
-        <Post author="Alan" body="Web developer" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Edin" body="Mobile repair shop owner" />
       </ul>
     </>
