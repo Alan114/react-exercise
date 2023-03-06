2aad5efb7006f613e23862c1232d60a3a360b76a
Alan Pecenkovic <alanquad@yahoo.com>
2023-03-06 15:10:58 +0100

    creating a Post component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <h1>React Poster</h1>
+      <Post author="Alan" body="Web developer" />
+      <Post author="Edin" body="Mobile repair shop owner" />
+    </main>
+  );
+}
+
+export default App;
