58b5e50235dbb6d63b24cbef0e79f9a9df5a56f7
Alan Pecenkovic <alanquad@yahoo.com>
2023-03-06 16:48:43 +0100

    rendering conditional content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import styles from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -16,11 +21,27 @@ function PostsList() {
     setEnteredAuthor(event.target.value);
   }
 
+  // let modalContent;
+
+  // if (modalIsVisible) {
+  //   modalContent = (
+  //     <Modal onClose={hideModalHandler}>
+  //       <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+  //     </Modal>
+  //   );
+  // }
+
   return (
     <>
-      <Modal>
-        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={styles.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Edin" body="Mobile repair shop owner" />
