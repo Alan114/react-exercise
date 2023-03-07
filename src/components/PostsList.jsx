c8cbfe434168e575eb8786c263f2152b82ec3ebd
Alan Pecenkovic <alanquad@yahoo.com>
2023-03-07 14:30:19 +0100

    handling form submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,45 +1,18 @@
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList({ modalIsVisible, hideModalHandler }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  // let modalContent;
-
-  // if (modalIsVisible) {
-  //   modalContent = (
-  //     <Modal onClose={hideModalHandler}>
-  //       <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
-  //     </Modal>
-  //   );
-  // }
-
   return (
     <>
       {modalIsVisible && (
         <Modal onClose={hideModalHandler}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={hideModalHandler}
-          />
+          <NewPost onCancel={hideModalHandler} />
         </Modal>
       )}
 
       <ul className={styles.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Edin" body="Mobile repair shop owner" />
       </ul>
     </>
