423c3731575cb46024c0c4e703033a0c7f7a3a98
Alan Pecenkovic <alanquad@yahoo.com>
2023-03-07 14:49:57 +0100

    outputting list data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
@@ -17,10 +18,19 @@ function PostsList({ modalIsVisible, hideModalHandler }) {
           <NewPost onCancel={hideModalHandler} onAddPost={addPostHandler} />
         </Modal>
       )}
-
-      <ul className={styles.posts}>
-        <Post author="Edin" body="Mobile repair shop owner" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post) => (
+            <Post author={post.author} body={post.body} key={Math.random()} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding them!</p>
+        </div>
+      )}
     </>
   );
 }
